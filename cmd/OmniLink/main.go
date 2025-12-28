385f3177fcfef482ee2d9a7a10604ce1afbad5d7
“Aurorakid0x” <3565477618@qq.com>
2025-12-28 19:42:00 +0800

    feat: 添加主程序入口并暂时禁用TLS重定向
    添加OmniLink主程序入口文件，实现HTTP服务启动和优雅关闭功能
    暂时注释掉https_server中的TLS重定向以支持HTTP运行

---
+++ cmd/OmniLink/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	https_server "OmniLink/api/http"
+	"OmniLink/internal/config"
+	"OmniLink/pkg/zlog"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// 1. 加载配置
+	conf := config.GetConfig()
+	host := conf.MainConfig.Host
+	port := conf.MainConfig.Port
+
+	// 2. 启动 HTTP 服务
+	go func() {
+		addr := fmt.Sprintf("%s:%d", host, port)
+		zlog.Info(fmt.Sprintf("服务器正在启动，监听地址: %s", addr))
+
+		// 目前使用 HTTP 启动。如果需要 HTTPS，请配置证书并使用 GE.RunTLS
+		// if err := https_server.GE.RunTLS(addr, "cert.pem", "key.pem"); err != nil {
+		if err := https_server.GE.Run(addr); err != nil {
+			zlog.Fatal("服务器启动失败: " + err.Error())
+			return
+		}
+	}()
+
+	// 3. 优雅关闭
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// 等待退出信号
+	<-quit
+
+	zlog.Info("正在关闭服务器...")
+	// 在此处添加资源释放逻辑（如关闭数据库连接、Redis 等）
+
+	zlog.Info("服务器已关闭")
+}
